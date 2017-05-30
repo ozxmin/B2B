65248abd97d0fb26a4237c6d0a9b8ef4187a2660
ozxmin <ozxmin@me.com>
2017-05-30 03:55:38 -0500

    Added private route for logged-in users

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,17 @@
+var {User} = require('./../models/user');
+
+let authenticate = (req, res, next) => {
+    let token = req.header('x-auth');
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject('Usuario no valido'); // res.status(401).send(error);
+        }
+        req.user = user;
+        req.token = token;
+        next();
+    }).catch((error) => {
+        res.status(401).send(error);
+    });
+};
+
+module.exports = {authenticate};
