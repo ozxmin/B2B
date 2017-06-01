cd7e21d96e2e79ef4b5ed6bd5a53850021b14562
ozxmin <ozxmin@me.com>
2017-06-01 00:35:53 -0500

    Bug corregido, version pasada no abria puerto

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -84,6 +84,10 @@ module.exports = function(app) {
         });
     });
 
+    app.listen(3000,() => {
+        console.log('Listening on port 3000');
+    });
+
 }
 
 
@@ -144,8 +148,6 @@ module.exports = function(app) {
 //         }
 //     }
 //
-//     app.listen(3000,() => {
-//         console.log('Listening on port 3000');
-//     });
+
 //
 // };
