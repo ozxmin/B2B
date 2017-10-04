ac51319e643720483cbf586ab7aaec7d07e18975
ozxmin <ozxmin@me.com>
2017-10-04 16:59:24 -0600

    Configuracion para Heroku

--- server/config.js
+++ server/config.js
@@ -9,17 +9,21 @@ if (env === 'dev') {
     //user.js – Generacion de Tokens
     process.env.ACCESS = 'access';
     process.env.SECRET_VALUE = 'valorsecreto';
-} else if (env === 'production') {
+} if (env === 'production') {
     //mongoosejs
     process.env.NODE_PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27013/b2busers';
     //user – Generacion de  Tokens
     process.env.ACCESS = 'ub1Wdx2aaOkZfqT7fFd3eAtT';
     process.env.SECRET_VALUE = 'PW7QoygcjCFX6bl5U9DtBtRX';
-} else if (env === 'test') {
+} if (env === 'test') {
     process.env.NODE_PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/B2BUsers';
     //user.js – Generacion de Tokens
     process.env.ACCESS = 'access';
     process.env.SECRET_VALUE = 'valorsecreto';
+} if (env == heroku ) {
+    process.env.MONGODB_URI = 'mongodb://m_lab_db_admin:vua-W4L-E87-uQ3@ds161164.mlab.com:61164/b2busers';
+    process.env.ACCESS = 'ub1Wdx2aaOkZfqT7fFd3eAtT';
+    process.env.SECRET_VALUE = 'PW7QoygcjCFX6bl5U9DtBtRX';
 }
